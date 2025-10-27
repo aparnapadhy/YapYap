519207c0bc62c7b798873a9ac4aadd8be10aa859
Aparna Padhy <aparnapadhyap672@gmail.com>
2025-10-27 20:52:43 +0530

    responsivness improved for mobile view

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -9,8 +9,8 @@ const HomePage = () => {
 const {selectedUser} = useContext(ChatContext);
 
   return (
-    <div className='max-w-[1400px]  h-screen sm:px-[15%] sm:py-[5%]'>
-       <div className={`backdrop-blur-xl border-2 border-pink-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser? 'md:grid-cols-[2fr_2fr_1.5fr]':'md:grid-cols-2'}`}>
+    <div className='max-w-[1400px]  h-screen sm:px-[15%] sm:py-[5%] flex justify-center items-center'>
+       <div className={`backdrop-blur-xl border-2 border-pink-600 rounded-2xl overflow-hidden w-5/6 h-[90%] sm:w-9/10 grid grid-cols-1 relative ${selectedUser? 'md:grid-cols-[2fr_2fr_1.5fr]':'md:grid-cols-2'}`}>
         <Sidebar />
         <ChatContainer/>
         <RightSidebar />
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -32,15 +32,15 @@ const handleSubmit = async(e)=>{
 }
 
   return (
-    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+    <div className='h-screen bg-cover bg-no-repeat flex items-center justify-center'>
 
-       <div className=' w-3/4 sm:w-full max-w-2xl backdrop-blur-2xl text-white border-2 border-pink-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+       <div className=' w-5/6  h-[90%] sm:w-9/10 max-w-2xl backdrop-blur-2xl text-white border-2 border-pink-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
 
         <form onSubmit={handleSubmit} className='flex flex-col gap-5 p-10 flex-1'>
           <h3 className='text-lg'>Profile Details</h3>
           <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
           <input onChange={(e)=>setSelectedImage(e.target.files[0])} type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden/>
-          <img src={selectedImage? URL.createObjectURL(selectedImage): assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImage && 'rounded-full'}`}/>
+          <img src={selectedImage? URL.createObjectURL(selectedImage): assets.avatar_icon} alt="" className={`w-5 h-5 ${selectedImage && 'rounded-full'}`}/>
           Upload Profile Image
           </label>
           <input onChange={(e)=>setName(e.target.value)} value={name} type="text" required placeholder='Your Name' className='p-2 border border-pink-400 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-400'/>
